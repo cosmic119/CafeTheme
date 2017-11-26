96c8fe82afdbed85489be5d8ac478226ac2f25de
Unknown <cosmic0119@gmail.com>
2017-11-26 19:37:19 +0900

    Activity 생성

---
+++ app/src/main/java/com/example/hyeongyu/caferecommend/ThemeSelectActivity.java
@@ -0,0 +1,13 @@
+package com.example.hyeongyu.caferecommend;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ThemeSelectActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_theme_select);
+    }
+}
